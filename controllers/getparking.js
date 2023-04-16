583beb1068890395a570bf17bd0b309fe9b02afd
Prajeeth <prajeeth347@gmail.com>
2023-04-16 14:41:10 +0530

    Find and update

--- controllers/getparking.js
+++ controllers/getparking.js
@@ -7,13 +7,14 @@ exports.getparking = async (req,res)=>{
         const address = await Parking.find();
         const found = false;
         console.log(address.length)
+
         for (let i = 0; i < address.length; i++){
             if(address[i].email == email){
                 res.status(200).json(address[i])
                 found = true;
             }
         }
-
+        if(found == false)
             res.status(404).json({message:"not found"});
     } catch (error) {
         console.error(error);
--- controllers/update_order.js
+++ controllers/update_order.js
@@ -6,8 +6,8 @@ exports.updateorders = async (req, res) => {
         const id = req.body.id
         const status = req.body.status
         const updatedSlots = await Parking.findByIdAndUpdate({ _id: id }, {status:status});
-        const final_updated = await updatedSlots.update();
-        res.status(201).json(final_updated)
+        // const final_updated = await updatedSlots.update();
+        res.status(201).json(updatedSlots)
     } catch (error) {
         console.log(error);
         res.status(404).json({ message: "fail", error: error });
